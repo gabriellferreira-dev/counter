6a29e868e83ce8f18e9ac1206ecc14161badd98c
Gabriel Ferreira <glucas709@gmail.com>
2022-03-02 18:43:35 -0300

    feat: counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,47 @@
-import Button from './Button';
+import { useState } from 'react';
+import Button from './components/Button';
 
 function App() {
+  const [count, setCount] = useState<number>(0);
+
+  const handleSubtract = () => {
+    if (count >= 0) setCount((prev) => prev - 1);
+  };
+
+  const handleAdd = () => setCount((prev) => prev + 1);
+
   return (
-    <div className="App">
-      <Button color="primary">-</Button>
-      <Button color="primary" icon="arrow-right">
-        -
-      </Button>
-      <Button color="primary" disabled>
-        -
-      </Button>
-      <Button color="secondary">-</Button>
-      <Button color="secondary" icon="arrow-right">
-        -
-      </Button>
-      <Button color="secondary" disabled>
-        -
-      </Button>
+    <div
+      className="App"
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+        backgroundColor: '#001520',
+      }}
+    >
+      <h1
+        style={{
+          fontSize: '30px',
+          color: '#fff',
+        }}
+      >
+        {count}
+      </h1>
+      <div
+        style={{
+          display: 'flex',
+        }}
+      >
+        <Button color="secondary" onClick={handleSubtract} disabled={!count}>
+          -
+        </Button>
+        <Button color="primary" onClick={handleAdd}>
+          +
+        </Button>
+      </div>
     </div>
   );
 }
